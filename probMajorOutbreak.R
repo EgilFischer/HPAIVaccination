867d627ad299505777c276b973cf8d6daa9bbcb6
Fischer <egil@egilfischer.nl>
2023-06-08 09:39:51 +0200

    Based on Clancy and Pearce added a method to determin pminor for gamma distributed Tinf

--- probMajorOutbreak.R
+++ probMajorOutbreak.R
@@ -12,12 +12,66 @@
 source("loadLibraries.R") 
 
 
-#probability generating functions q1, q2 for offspring of type 1 or  2, given ultimate extinction e.g. I1(\[Infinity]) = 0;  I2(\[Infinity]) =0
-model <- function(u, param.list) {with((param.list),
+#Function to find the root of q1,q2 = pgf (probability generating functions) for q1, q2 for offspring of type 1 or  2, given ultimate extinction e.g. I1(\[Infinity]) = 0;  I2(\[Infinity]) =0
+#In this function f1 = -u1 + pgf1 and f2 = -u2 + pgf2 
+#exponential distributed infectious period
+model.exp <- function(u, param.list) {with((param.list),
   return(c(f1 =-u[1] + (gamma[1] + mu + beta[1,1]* (N[1]/(N[1] +N[2]) )* u[1]^2 + beta[2,1]* (N[2] /(N[1] + N[2]))* u[1]* u[2])/(gamma[1]+ mu + beta[1,1]* (N[1]/(N[1] + N[2]))+ beta[2,1]* N[2] /(N[1] + N[2])),
                        f2 = -u[2] + (gamma[2] + mu + beta[1,2]* (N[1]/(N[1] +N[2]) )* u[1] * u[2] + beta[2,2]* (N[2] /(N[1] + N[2]))* u[2]^2)/(gamma[2]+ mu + beta[1,2]* (N[1]/(N[1] + N[2]))+ beta[2,2]* N[2] /(N[1] + N[2])))))}
 
-Rmodel <- function(param.list, p){with(param.list,
+#gamma distributed infectious period - disregarding mortality
+#Laplace Transform of gamma distributed infectious period which z is the infection rate 
+# rm(L)
+# L<- function(z,i, param.list){with(param.list,{
+#   a <- infectious.period[i]^2 / variance.infectious.period[i]; #shape
+#   b <- variance.infectious.period[i]/infectious.period[i];  #scale
+#   return((b)^-a * ((1 / b) + z)^-a)
+#   })
+# }
+
+#From Clancy and Pearce 2013
+model.gamma <- function(u, param.list) {
+  with((param.list),{
+  #scale parameters to mean infectious period 
+  R = beta*infectious.period;
+  #shape parameter for infectious period
+  a = infectious.period^2 / variance.infectious.period;
+  #determine fraction in each population
+  p = c(N[1]/sum(N), N[2]/sum(N));
+
+  return(c(f1 =-u[1] + (1 + ((1 - u[1]) * p[1] * R[1, 1] + (1 - u[2]) * p[2] * R[2, 1])/a[1])^-a[1], #type 1
+          f2 = -u[2] + (1 + ((1 - u[1]) * p[1] * R[1, 2] + (1 - u[2]) * p[2] * R[2, 2])/a[2])^-a[2])) #type 2
+       }
+    )}
+
+
+
+# model.gamma <- function(u, param.list) {
+#   with((param.list),{
+#     #transfrom beta into R value
+# 
+#   return(c(f1 =-u[1] + L(beta[1,1]* (N[1]/(N[1] +N[2])) + beta[2,1]* (N[2] /(N[1] + N[2])), 1,param.list) #recovery of one without infecting another individual
+#            + (1-L(beta[1,1]* (N[1]/(N[1] +N[2])) + beta[2,1]* (N[2] /(N[1] + N[2])),1,param.list)) * #no recovery
+#              (u[1]^2 *  beta[1,1]* (N[1]/(N[1] +N[2]))     +  # infect type 1
+#              u[1]*u[2] *beta[2,1]* (N[2] /(N[1] + N[2])))/ #infect type 2
+#              (beta[1,1]* (N[1]/(N[1] +N[2])) + beta[2,1]* (N[2] /(N[1] + N[2]))) #normalize
+#              ,
+#             f2 = -u[2]  + L(beta[1,2]* (N[1]/(N[1] +N[2])) + beta[2,2]* (N[2] /(N[1] + N[2])), 2,param.list) #recovery of one without infecting another individual
+#             + (1-L(beta[1,2]* (N[1]/(N[1] +N[2])) + beta[2,2]* (N[2] /(N[1] + N[2])),2,param.list)) * #no recovery
+#               (u[1]*u[2] *   beta[1,2]* (N[1]/(N[1] +N[2]))     +  #infect type 1
+#                   u[2]^2 *beta[2,2]* (N[2] /(N[1] + N[2])))/ #infect type 2
+#                  (beta[1,2]* (N[1]/(N[1] +N[2])) + beta[2,2]* (N[2] /(N[1] + N[2]))) #normalize
+#       ))       }
+#     )}
+
+
+
+#select the model to use
+model <- model.exp
+  
+
+#
+Rmodel.exp <- function(param.list, p){with(param.list,
   { N <- c(1-p,p)*N0;
     return(0.5*(beta[1,1]*(N[1]/(N[1]+N[2]))/(gamma[1]+mu) + 
                   beta[2,2]*(N[2]/(N[1]+N[2])) /(gamma[2]+mu) + 
@@ -25,32 +79,88 @@ Rmodel <- function(param.list, p){with(param.list,
                        4 * (beta[1,2]*(N[2]/(N[1]+N[2]))*beta[2,1]*(N[1]/(N[1]+N[2])))/((gamma[2]+mu)*(gamma[1]+mu)))))
   })}
 
+Rmodel.gamma <- function(param.list, p){with(param.list,
+                                       { N <- c(1-p,p)*N0;
+                                       return(0.5*(beta[1,1]*(N[1]/(N[1]+N[2])) * infectious.period[1] + 
+                                                     beta[2,2]*(N[2]/(N[1]+N[2]))* infectious.period[2] + 
+                                                     sqrt((beta[2,2]*(N[2]/(N[1]+N[2]))* infectious.period[2] - beta[1,1]*(N[1]/(N[1]+N[2]))* infectious.period[1])^2 + 
+                                                            4 * (beta[1,2]*(N[2]/(N[1]+N[2]))*infectious.period[1]*beta[2,1]*(N[1]/(N[1]+N[2])))*infectious.period[2])))
+                                       })}
+
 q1q2 <- function(param.list){
-  dat<- data.frame(t(sapply(FUN = function(p){c(p,multiroot(f = model, start = c(.01,.1),
-                                                      rtol = 1E-15,
+  dat<- data.frame(t(sapply(FUN = function(p){c(p,multiroot(f = model, start = c(.01,.01),
+                                                      rtol = 1E-17,
                                                       parms = c(param.list,N = list(param.list$N0*c(1-p,p))))$root)}, X = seq(0,1,.01))))
   dat$Rv <- sapply(FUN = Rmodel, param.list = param.list,X = seq(0,1,.01) )
   names(dat)<- c("p","q1","q2","Rv");
   return(dat)
 }
 
-# q1q2 <- data.frame(t(sapply(FUN = function(p){c(p,multiroot(f = model, start = c(.01,
-#                                                                                 0.1),
-#                                                       rtol = 1E-15,parms = list(N = c(10000 * (1-p),10000 * p),
-#                                                                                gamma = c(1/1.47,1/1.47), beta = matrix(c(3.84,3.84,0.058,0.058),nrow = 2), mu = 0.0005))$root)}, X = seq(0,1,.05))))
-# q1q2
-# names(q1q2)<- c("p","q1","q2")
-# 
-# #initial number of infected animals per group
-# i1 = 5;
-# i2 = 5;
-# 
-# ggplot(data =q1q2)+
-#   geom_path(aes(p,1 - q1^(i1+i2), colour = "1"))+
-#   geom_path(aes(p,1-  q2^(i1+i2), colour = "2"))+
-#   geom_path(aes(p,1-(q1^i1* q2^i2), colour = "1&2"))+
-#   xlab("Vaccinated coverage")+
-#   ylab("Probability of a major outbreak")+ 
-#   scale_colour_manual(name = paste("Introduction by",i1+i2,"birds"),
-#                       labels = c("Type 1 only","Type 2 only","Type 1 & 2"),
-#                       values = c("red","blue","black"))
+#test the exponential and gamma are equal if the variance is the square of the mean (e.g. shape has value 1)
+param.list <- param.list.baseline.layer
+param.list$gamma <- 1/param.list$infectious.period
+param.list$mu <- param.list$mortRate
+#exponential distributed infectious period
+model <- model.exp
+Rmodel <- Rmodel.exp
+q1q2.exp <- q1q2(param.list)
+#exponential distributed infectious period using gamma distribution with shape = 1
+param.list$variance.infectious.period <- param.list$infectious.period^2
+model <- model.gamma
+Rmodel <- Rmodel.gamma
+q1q2.gamma <-q1q2(param.list)
+#gamma distributed infectious period using gamma distribution with shape = 20
+param.list <- param.list.baseline.layer
+q1q2.gamma.var <-q1q2(param.list)
+
+
+#combine 
+q1q2 <- rbind(cbind(q1q2.exp,data.frame(infectiousperiod = "exponential")),
+                    cbind(q1q2.gamma,data.frame(infectiousperiod = "gamma.exp")),
+              cbind(q1q2.gamma.var,data.frame(infectiousperiod = "gamma.var")))
+
+ 
+#initial number of infected animals per group
+ i1 = .5;
+ i2 = .5;
+ 
+ ggplot(data =q1q2)+
+   geom_path(aes(p, q1^(i1+i2), colour = "1",linetype = infectiousperiod), linewidth = 1.5)+
+   geom_path(aes(p, q2^(i1+i2), colour = "2",linetype = infectiousperiod), linewidth = 1.5)+
+   geom_path(aes(p,(q1^i1* q2^i2), colour = "1&2",linetype = infectiousperiod), linewidth = 1.5)+
+   geom_path(aes(p,Rv, colour = "R",linetype = infectiousperiod), linewidth = 1.5)+
+   xlab("Vaccinated coverage")+
+   ylab("Probability of a minor outbreak")+ 
+     scale_colour_manual(name = paste("Introduction by",i1+i2,"birds"),
+                         labels = c("Type 1 only","Type 1 & 2","Type 2 only","R"),
+                         values = c("red","blue","green","black"))#+
+  #facet_grid(infectiousperiod~.)
+
+   
+
+
+#debugging
+param.list$variance.infectious.period<- param.list$infectious.period^2
+model.gamma.debug <- function(u1,u2, param.list){model.gamma(c(u1,u2),param.list)}
+model.gamma.debug(0,1,c(param.list,N = list(c(0.75,0.25))))
+test <- data.frame(u1 = rep(seq(0,1,.1),10),u2 = rep(seq(0,1,.1),each =10))
+ 
+f = mapply(model.gamma.debug, u1 = test$u1, u2 =test$u2, MoreArgs = list(param.list = c(param.list,N = list(c(0.75,0.25))) ))
+test$f1 <-t(f)[,1]
+test$f2 <-t(f)[,2]
+
+ggplot(test)+geom_path(aes(u1,f1,colour = as.factor(u2)))#+theme(legend.position = "none")
+ggplot(test)+geom_path(aes(u2,f2,colour = as.factor(u1)))#+theme(legend.position = "none")
+
+multiroot(f = model.gamma, start = c(.01,.1),
+          rtol = 1E-50,
+          parms = c(param.list,N = list(param.list$N0*c(.75,.25))))
+
+param.list$beta <- matrix(c(2,.5,2,.5),nrow =2)
+param.list$infectious.period <- c(1,1)
+param.list$variance.infectious.period <- c(1,1)
+p  = 1.
+multiroot(f = model, start = c(.9,0.9),
+          rtol = 1E-17,
+          parms = c(param.list,N = list(param.list$N0*c(1-p,p))))
+
