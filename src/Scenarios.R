719c041603776801c37cf1638e3436b5f56e6df0
Fischer <egil@egilfischer.nl>
2023-06-29 12:19:52 +0200

    Baseline Scenarios without waning

--- src/Scenarios.R
+++ src/Scenarios.R
@@ -57,7 +57,7 @@ itypes = 2;
 #Baseline parameters for layer flock ####
 param.list.baseline.layer <- list(
   scenario = "baseline_Layer", #scenario
-  runs = 40, #number of runs
+  runs = 10, #number of runs
   max.time = 17*30,#length of the run
   itypes = itypes, #type
   N0 = 45000, #population size - agramatie website
@@ -66,7 +66,7 @@ param.list.baseline.layer <- list(
   beta = matrix(c(1.13, 1.13,0.05,0.05),ncol = itypes),#,#transmission coefficient matrix for a 2x2 matrix (1 -> 1, 1->2, 2-> 1, 2-> 2)#Use values for infectivity and infectious periods from Sitaris et al 2016 https://doi.org/10.1098/rsif.2015.0976 and Gemeraard et al 2023 #Type 1  = not protected by vaccination and type 2 = protected by vaccination
   infectious.period = c(3.0,4.0),#Duration infectious period 
   variance.infectious.period = c(3.0,4.0)^2/20, #Variance infectious period - Hobbelen et al uses shape parameter of 20 -> variance = m^2 / shape 
-  transRate = matrix(c(0,0.038,0.0,0), nrow = itypes), #value based on https://nvaidya.sdsu.edu/SIAP2015.pdf #transition rates should be of size itypes x itypes with 0 on diagonal
+  transRate = matrix(c(0,0.0,0.0,0), nrow = itypes), #value based on https://nvaidya.sdsu.edu/SIAP2015.pdf #transition rates should be of size itypes x itypes with 0 on diagonal
   pdie = c(0.99,0.01),#probability of dying at end of infectious period
   mortRate = 0.0005/7 #per capita death rate #Mortality events - based on performance reports and pers. com. mieke matthijs 0.5% per week +Gonzales & elbers figure 1 
 )
